14d03c5ea32e1da9ce671892ec3c19d0922ea00b
edelmutic <edelmutic@gmail.com>
2021-04-21 13:10:28 +0300

    fix styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
-  return <>Hello</>;
+  const [users, setUsers] = useState([])
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="number" />
+        <div className='button'>
+          <button>Create</button>
+          <button>Receive</button>
+        </div>
+      </form>
+      <div>
+        {users.map((user) => {
+          <div className='user'>{user.id} {user.name} {user.age}</div>
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default App;
