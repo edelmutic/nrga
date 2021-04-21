a7b2abe9033f04ed07c57c95ac2e96dcf73dc4d1
edelmutic <edelmutic@gmail.com>
2021-04-21 12:48:15 +0300

    add apollo provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
